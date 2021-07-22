67172a1e2af1bf83c7c1e9d87e4e97ec409c4ca2
vlad_zavdv <vladzavdv16@gmail.com>
2021-07-22 22:00:52 +0300

    changed API_KEY

--- app/src/main/java/com/light/loftcoin/data/CmcApi.java
+++ app/src/main/java/com/light/loftcoin/data/CmcApi.java
@@ -7,8 +7,9 @@ import retrofit2.http.Query;
 
 public interface CmcApi {
 
-    String API_KEY = "CMC_PRO_API_KEY";
+    String API_KEY = "X-CMC_PRO_API_KEY";
 
     @GET("cryptocurrency/listings/latest")
-    Call<Listings>listings(@Query("convert") String convert);
+    Call<Listings> listings(@Query("convert") String convert);
+
 }
